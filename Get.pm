c36625a1981d02fb3e959e67a6757b06d59a7016
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-01-06 10:49:55 +0100

    Prepare for 0.06 version

--- Get.pm
+++ Get.pm
@@ -11,7 +11,7 @@ use LWP::UserAgent;
 use Menlo::Index::MetaCPAN;
 use URI::cpan;
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -214,6 +214,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
